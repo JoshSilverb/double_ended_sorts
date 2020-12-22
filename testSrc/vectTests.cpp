d5d75a84c7641bfc65d7ca5ed16dc8dbe70ab6bd
joshsilv <joshsilv@umich.edu>
2020-12-22 16:20:57 +0100

    Finished dbs & added dbs vectTests

--- testSrc/vectTests.cpp
+++ testSrc/vectTests.cpp
@@ -1,4 +1,5 @@
 #include "double_selection_sort.h"
+#include "double_bubble_sort.h"
 #include <vector>
 #include <random>
 #include <iostream>
@@ -39,28 +40,36 @@ int main(int argc, char** argv){
 void basic_int(){
     cout << "Running basic_int()" << endl;
     double_selection_sort<int> dss;
+    double_bubble_sort<int> dbs;
     size_t size = 10;
 
-    vector<int> dataVec;
+    vector<int> dataVec1;
+    vector<int> dataVec2;
     cout << "dataVec: ";
     for(int i=0; i<size; ++i){
         int pushVal = rand() % 100;
-        dataVec.push_back(pushVal);
+        dataVec1.push_back(pushVal);
         cout << pushVal << " ";
     }
+    dataVec2 = dataVec1;
     cout << endl;
 
-    dss(dataVec);
+    dss(dataVec1);
+    dbs(dataVec2);
 
-    cout << "Vector: ";
+    cout << "Vectors:\n";
     for(int i=0; i<size; ++i){
-        cout << dataVec[i] << " " << flush;
-        //cout << flush;
+        cout << dataVec1[i] << " ";
+    }
+    cout << endl;
+    for(int i=0; i<size; ++i){
+        cout << dataVec2[i] << " ";
     }
     for(int i=0; i<size; ++i){
-        if(i>0) assert(dataVec[i] >= dataVec[i-1]);
-        //cout << flush;
+        if(i>0) assert((dataVec1[i] >= dataVec1[i-1]) && (dataVec2[i] >= dataVec2[i-1]));
     }
+
+
     cout << endl;
     cout << "Completed basic_int()" << endl;
 
@@ -73,21 +82,31 @@ void basic_custom_object(){
     DuckLess dcomp;
 
     double_selection_sort<Duck, DuckLess> dss;
+    double_bubble_sort<Duck, DuckLess> dbs;
+
     size_t size = 10;
 
-    vector<Duck> ducks;
+    vector<Duck> ducks1;
+    vector<Duck> ducks2;
 
     for(int i=0; i<size; ++i){
-        ducks.emplace_back(rand() % 100, (char)((int)'a' + i));
+        ducks1.emplace_back(rand() % 100, (char)((int)'a' + i));
     }
+    ducks2 = ducks1;
 
-    dss(ducks);
+    dss(ducks1);
+    dbs(ducks2);
 
     cout << "Vector: ";
     for(int i=0; i<size; ++i){
-        cout << ducks[i].age << " " << ducks[i].letter << " ";
-        if(i>0) assert(dcomp(ducks[i-1], ducks[i]) 
-                  || (!dcomp(ducks[i-1], ducks[i]) && !dcomp(ducks[i], ducks[i-1])));
+        cout << ducks1[i].age << " " << ducks1[i].letter << " ";
+        if(i>0) assert(dcomp(ducks1[i-1], ducks1[i]) 
+                  || (!dcomp(ducks1[i-1], ducks1[i]) && !dcomp(ducks1[i], ducks1[i-1])));
+    }
+    for(int i=0; i<size; ++i){
+        cout << ducks2[i].age << " " << ducks2[i].letter << " ";
+        if(i>0) assert(dcomp(ducks2[i-1], ducks2[i]) 
+                  || (!dcomp(ducks2[i-1], ducks2[i]) && !dcomp(ducks2[i], ducks2[i-1])));
     }
     cout << endl;
     cout << "Completed basic_custom_object() successfully" << endl;
@@ -99,15 +118,24 @@ void extra_test(){
     cout << "Running extra_test()" << endl;
 
     //vector<int> vec = {4, 92, 21, 77, 69, 52, 22, 87, 5, 93};
-    vector<int> vec = {66, 60, 55, 9, 65, 57, 49, 54, 89, 80};
+    vector<int> vec1 = {66, 60, 55, 9, 65, 57, 49, 54, 89, 80};
     double_selection_sort<int> dss;
+    double_bubble_sort<int> dbs;
+    
+    vector<int> vec2 = vec1;
 
-    dss(vec);
+    dss(vec1);
+    dbs(vec2);
 
     cout << "Vector: ";
     for(int i=0; i<10; ++i){
-        cout << vec[i] << " ";
-        if(i>0) assert(vec[i] >= vec[i-1]);
+        cout << vec1[i] << " ";
+        if(i>0) assert(vec1[i] >= vec1[i-1]);
+        cout << flush;
+    }
+    for(int i=0; i<10; ++i){
+        cout << vec2[i] << " ";
+        if(i>0) assert(vec2[i] >= vec2[i-1]);
         cout << flush;
     }
     cout << endl;
@@ -119,30 +147,42 @@ void extra_test(){
 void small_vector(){
     cout << "Running small_vector()" << endl;
     double_selection_sort<int> dss;
+    double_bubble_sort<int> dbs;
     unsigned int sizes[3] = {3, 2, 1};
 
-    vector<int> dataVec;
+    vector<int> dataVec1;
+    vector<int> dataVec2;
 
     for(unsigned int size : sizes){
         cout << "Starting test for size = " << size << endl;
         cout << "dataVec: ";
         for(int i=0; i<size; ++i){
             int pushVal = rand() % 100;
-            dataVec.push_back(pushVal);
+            dataVec1.push_back(pushVal);
             cout << pushVal << " ";
         }
         cout << endl;
+        dataVec2 = dataVec1;
 
-        dss(dataVec);
+        dss(dataVec1);
+        dbs(dataVec2);
 
-        cout << "Vector: ";
+        cout << "Vector:\n";
         for(int i=0; i<size; ++i){
-            cout << dataVec[i] << " " << flush;
+            cout << dataVec1[i] << " ";
         }
+        cout << endl;
         for(int i=0; i<size; ++i){
-            if(i>0) assert(dataVec[i] >= dataVec[i-1]);
+            cout << dataVec2[i] << " ";
+        }
+        cout << endl;
+
+        for(int i=0; i<size; ++i){
+            if(i>0) assert(dataVec1[i] >= dataVec1[i-1] && dataVec2[i] >= dataVec2[i-1]);
         }
         cout << "Passed size = " << size << endl;
+        dataVec1.clear();
+        dataVec2.clear();
     }
     cout << "Completed small_vector()" << endl;
 }
@@ -150,22 +190,32 @@ void small_vector(){
 void big_vector(){
     cout << "Running big_vector()" << endl;
     double_selection_sort<int> dss;
+    double_bubble_sort<int> dbs;
     unsigned int sizes[4] = {100, 256, 1028, 65536};
 
-    vector<int> dataVec;
+    vector<int> dataVec1;
+    vector<int> dataVec2;
     for(unsigned int size : sizes){
         cout << "Starting test for size = " << size << endl;
         for(int i=0; i<size; ++i){
-            dataVec.push_back(rand() % 100);
+            dataVec1.push_back(rand() % 100);
         }
+        dataVec2 = dataVec1;
 
-        dss(dataVec);
-
+        dss(dataVec1);
+        cout << "dss finished\n";
         for(int i=0; i<size; ++i){
-            if(i>0) assert(dataVec[i] >= dataVec[i-1]);
+            if(i>0) assert(dataVec1[i] >= dataVec1[i-1] && dataVec2[i] >= dataVec2[i-1]);
         }
+        dbs(dataVec2);
+        cout << "dbs finished\n";
+        for(int i=0; i<size; ++i){
+            if(i>0) assert(dataVec1[i] >= dataVec1[i-1] && dataVec2[i] >= dataVec2[i-1]);
+        }
+        
         cout << "Passed size = " << size << endl;
-        dataVec.clear();
+        dataVec1.clear();
+        dataVec2.clear();
     }
     cout << "Completed big_vector()" << endl;
 }
